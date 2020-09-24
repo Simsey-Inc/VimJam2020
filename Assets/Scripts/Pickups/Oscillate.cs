4de8eb75abbf52801ac32514ea2880ec8b02d8fc
ramseymardini <ramseymardini@gmail.com>
2020-09-24 14:17:57 -0700

    Finished battery pickups

---
+++ Assets/Scripts/Pickups/Oscillate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillate : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private float frequency;
+
+    [SerializeField]
+    private float amplitude;
+    #endregion
+
+    #region Private Variables
+    private Rigidbody2D rb;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(0, amplitude * Mathf.Sin(Time.timeSinceLevelLoad * frequency));
+    }
+}
---
+++ Assets/Scripts/Player/PickupHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PickupHandler : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private Light2D flashlight;
+
+    [SerializeField]
+    private float amtIncreaseInnerAngle;
+
+    [SerializeField]
+    private float amtIncreaseOuterAngle;
+
+    [SerializeField]
+    private float amtIncreaseInnerRadius;
+
+    [SerializeField]
+    private float amtIncreaseOuterRadius;
+
+    [SerializeField]
+    private float amtIncreaseIntensity;
+
+    #endregion
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("BatteryPickup")) {
+            flashlight.pointLightInnerAngle += amtIncreaseInnerAngle;
+            flashlight.pointLightOuterAngle += amtIncreaseInnerAngle;
+
+            flashlight.pointLightInnerRadius += amtIncreaseInnerRadius;
+            flashlight.pointLightOuterRadius += amtIncreaseOuterRadius;
+            flashlight.intensity += amtIncreaseIntensity;
+            Destroy(other.gameObject);
+        }
+    }
+}
