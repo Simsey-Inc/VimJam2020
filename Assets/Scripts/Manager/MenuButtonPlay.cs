d8a22ddc7f15fe949e5d9198ab3216e647807b7b
ramseymardini <ramseymardini@gmail.com>
2020-09-25 00:26:14 -0700

    Added music stuff

--- Assets/Scripts/Manager/MenuButtonPlay.cs
+++ Assets/Scripts/Manager/MenuButtonPlay.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonPlay : MonoBehaviour
 {
+    #region Editor Variables
+    [SerializeField]
+    private MusicManager musicManager;
+    #endregion
+
     public void PlayGame()
     {
+        musicManager.PlayMainSong();
         SceneManager.LoadScene("Intro");
     }
 
--- Assets/Scripts/Manager/MusicManager.cs
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -7,11 +7,50 @@ public class MusicManager : MonoBehaviour
     #region Editor Variables
     [SerializeField]
     private List<AudioSource> music;
+
+    [SerializeField]
+    private int framesToPhase;
+    #endregion
+
+    #region Private Variables
+    private AudioSource currentlyPlaying;
     #endregion
 
     private void Awake()
     {
         music[0].Play();
+        currentlyPlaying = music[0];
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("MusicManager").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMainSong()
+    {
+        StartCoroutine(PhaseInTrack(music[1]));
+    }
+
+    private IEnumerator PhaseInTrack(AudioSource newSong)
+    {
+        float maxVolNewSong = newSong.volume;
+        newSong.volume = 0;
+        float amountToDecreaseOldSong = currentlyPlaying.volume / framesToPhase;
+        float amountToIncreaseNewSong = maxVolNewSong / framesToPhase;
+
+        newSong.Play();
+        for (int i = 0; i < framesToPhase; i++)
+        {
+            currentlyPlaying.volume -= amountToDecreaseOldSong;
+            newSong.volume += amountToIncreaseNewSong;
+            yield return new WaitForEndOfFrame();
+        }
+        currentlyPlaying.Stop();
+        currentlyPlaying = newSong;
     }
 }
