2be4d6ce31fa7a089b1b67961a4b87e37175265a
ramseymardini <ramseymardini@gmail.com>
2020-09-25 00:05:21 -0700

    Added music manager

---
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private List<AudioSource> music;
+    #endregion
+
+    private void Awake()
+    {
+        music[0].Play();
+        DontDestroyOnLoad(gameObject);
+    }
+}
