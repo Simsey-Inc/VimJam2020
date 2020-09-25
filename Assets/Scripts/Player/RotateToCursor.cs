6ec4bfec545e8f8e6ce47b770e46ef11bfc45f04
ramseymardini <ramseymardini@gmail.com>
2020-09-24 22:55:35 -0700

    Fixing player arm movement

--- Assets/Scripts/Player/RotateToCursor.cs
+++ Assets/Scripts/Player/RotateToCursor.cs
@@ -9,9 +9,6 @@ public class RotateToCursor : MonoBehaviour
     private Transform shoulderTransform;
 
     [SerializeField]
-    private Transform flashlightStartingTransform;
-
-    [SerializeField]
     private float flashlightRotationSensitivity;
 
     [SerializeField]
@@ -23,11 +20,30 @@ public class RotateToCursor : MonoBehaviour
 
     #region Private Variables
     private Vector2 startingDifferenceVector;
+    
+    private float currRotation;
     #endregion
+
+    private void Start()
+    {
+        currRotation = 0;
+    }
     
     private void Update()
     {
-        transform.RotateAround(shoulderTransform.position, Vector3.forward, flashlightRotationSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime);
+        float amountToRotate = Mathf.Clamp(flashlightRotationSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime, maxRotationDown / 4, maxRotationUp / 4);
+        Debug.Log("Amount to Rotate:" + amountToRotate);
+        Debug.Log("Current Rotation: " + currRotation);
+        if (currRotation + amountToRotate >= maxRotationUp)
+        {
+            amountToRotate = maxRotationUp - currRotation;
+        }
+        else if (currRotation + amountToRotate <= maxRotationDown)
+        {
+            amountToRotate = maxRotationDown - currRotation;
+        }
+        currRotation += amountToRotate;
+        transform.RotateAround(shoulderTransform.position, Vector3.forward, amountToRotate);
     }
 
     
